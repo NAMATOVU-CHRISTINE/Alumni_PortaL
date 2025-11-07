68b81e7642203f9a1f8ec1a6bcb7dc0c555f4c55
Christine <2023bcs004@std.must.ac.ug>
2025-11-07 04:10:43 +0300

    feat: Create CreateEventActivity and EventDetailsActivity with basic UI layout

---
+++ app/src/main/java/com/namatovu/alumniportal/database/AppDatabase.java
@@ -0,0 +1,38 @@
+package com.namatovu.alumniportal.database;
+
+import android.content.Context;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import com.namatovu.alumniportal.database.dao.ChatMessageDao;
+import com.namatovu.alumniportal.database.dao.EventDao;
+import com.namatovu.alumniportal.database.dao.JobPostingDao;
+import com.namatovu.alumniportal.database.dao.UserDao;
+import com.namatovu.alumniportal.database.entities.ChatMessageEntity;
+import com.namatovu.alumniportal.database.entities.EventEntity;
+import com.namatovu.alumniportal.database.entities.JobPostingEntity;
+import com.namatovu.alumniportal.database.entities.UserEntity;
+
+@Database(entities = {UserEntity.class, JobPostingEntity.class, EventEntity.class, ChatMessageEntity.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract UserDao userDao();
+    public abstract JobPostingDao jobPostingDao();
+    public abstract EventDao eventDao();
+    public abstract ChatMessageDao chatMessageDao();
+
+    private static volatile AppDatabase INSTANCE;
+
+    public static AppDatabase getInstance(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                                    AppDatabase.class, "alumni_portal_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
