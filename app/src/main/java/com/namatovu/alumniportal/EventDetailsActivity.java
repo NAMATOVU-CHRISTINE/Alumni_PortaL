633b32f4be48a2979664618df1a9c5b7afc13f38
Christine <2023bcs004@std.must.ac.ug>
2025-11-07 04:10:43 +0300

    feat: Create CreateEventActivity and EventDetailsActivity with basic UI layout

---
+++ app/src/main/java/com/namatovu/alumniportal/EventDetailsActivity.java
@@ -0,0 +1,13 @@
+package com.namatovu.alumniportal;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_details);
+    }
+}
