0408b98cb28db554c68485e211a24deb5876bcb3
Christine <2023bcs004@std.must.ac.ug>
2025-11-17 06:03:34 +0300

    Theme

---
+++ app/src/main/java/com/namatovu/alumniportal/utils/ThemeManager.java
@@ -0,0 +1,70 @@
+package com.namatovu.alumniportal.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class ThemeManager {
+    private static final String PREFS_NAME = "theme_prefs";
+    private static final String KEY_THEME = "app_theme";
+    
+    public static final int THEME_LIGHT = 0;
+    public static final int THEME_DARK = 1;
+    public static final int THEME_SYSTEM = 2;
+    
+    private static ThemeManager instance;
+    private SharedPreferences prefs;
+    
+    private ThemeManager(Context context) {
+        prefs = context.getApplicationContext().getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+    }
+    
+    public static ThemeManager getInstance(Context context) {
+        if (instance == null) {
+            instance = new ThemeManager(context);
+        }
+        return instance;
+    }
+    
+    public void setTheme(int theme) {
+        prefs.edit().putInt(KEY_THEME, theme).apply();
+        applyTheme(theme);
+    }
+    
+    public int getTheme() {
+        return prefs.getInt(KEY_THEME, THEME_SYSTEM);
+    }
+    
+    public String getThemeName() {
+        int theme = getTheme();
+        switch (theme) {
+            case THEME_LIGHT:
+                return "Light";
+            case THEME_DARK:
+                return "Dark";
+            case THEME_SYSTEM:
+                return "System Default";
+            default:
+                return "System Default";
+        }
+    }
+    
+    public void applyTheme(int theme) {
+        switch (theme) {
+            case THEME_LIGHT:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+            case THEME_DARK:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            case THEME_SYSTEM:
+            default:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
+        }
+    }
+    
+    public void applySavedTheme() {
+        applyTheme(getTheme());
+    }
+}
