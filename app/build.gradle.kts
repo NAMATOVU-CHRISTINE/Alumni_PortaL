6b8283aff3db66e77bf867094ab3d3a3b70771c1
Christine <2023bcs004@std.must.ac.ug>
2025-11-25 17:01:52 +0300

    Bump version to 1.3.4 (build 9) - authentication fix attempt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.namatovu.alumniportal"
         minSdk = 23
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.3.3"
+        versionCode = 9
+        versionName = "1.3.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
