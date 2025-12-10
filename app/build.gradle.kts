bd6073552bf84f5af74ade8272126b3999d776df
Christine <2023bcs004@std.must.ac.ug>
2025-12-10 21:17:42 +0300

    (versionCode 15).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.namatovu.alumniportal"
         minSdk = 23
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.3.9"
+        versionCode = 15
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
