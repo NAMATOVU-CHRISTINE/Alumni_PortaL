df379eb3b901cf18451be2f4fd84f13a525476e0
Christine <2023bcs004@std.must.ac.ug>
2025-12-11 11:06:00 +0300

    Delete account URL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.namatovu.alumniportal"
         minSdk = 23
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.4.1"
+        versionCode = 17
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
