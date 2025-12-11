13f4c2a3c3725a5802e2d1a7b903d2ea74847fad
Christine <2023bcs004@std.must.ac.ug>
2025-12-11 08:16:27 +0300

    Updated to version 1.4.1 (versionCode 16).

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "com.namatovu.alumniportal"
         minSdk = 23
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.4.0"
+        versionCode = 16
+        versionName = "1.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
