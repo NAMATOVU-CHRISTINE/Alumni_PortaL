39048e001a9266f0dc6fec0fb190643274eb948c
NAMATOVU-CHRISTINE <2023bcs004@std.must.ac.ug>
2026-02-10 12:06:39 +0300

    Fix AuthProvider naming conflict and remove web-only setPersistence

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:alumni_portal/firebase_options.dart';
 import 'package:alumni_portal/config/theme.dart';
 import 'package:alumni_portal/config/routes.dart';
-import 'package:alumni_portal/providers/auth_provider.dart';
+import 'package:alumni_portal/providers/auth_provider.dart' as app_auth;
 import 'package:alumni_portal/providers/user_provider.dart';
 import 'package:alumni_portal/providers/chat_provider.dart';
 import 'package:alumni_portal/providers/notification_provider.dart';
@@ -16,11 +17,17 @@ import 'package:alumni_portal/providers/poll_provider.dart';
 import 'package:alumni_portal/providers/story_provider.dart';
 import 'package:alumni_portal/providers/endorsement_provider.dart';
 import 'package:alumni_portal/providers/referral_provider.dart';
+import 'package:alumni_portal/providers/convocation_provider.dart';
+import 'package:alumni_portal/providers/guest_lecture_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Note: Firebase Auth persistence is automatic on mobile platforms
+  // setPersistence() is only for web
+
   runApp(const AlumniPortalApp());
 }
 
@@ -31,7 +38,7 @@ class AlumniPortalApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => app_auth.AuthProvider()),
         ChangeNotifierProvider(create: (_) => UserProvider()),
         ChangeNotifierProvider(create: (_) => ChatProvider()),
         ChangeNotifierProvider(create: (_) => NotificationProvider()),
@@ -42,6 +49,8 @@ class AlumniPortalApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => StoryProvider()),
         ChangeNotifierProvider(create: (_) => EndorsementProvider()),
         ChangeNotifierProvider(create: (_) => ReferralProvider()),
+        ChangeNotifierProvider(create: (_) => ConvocationProvider()),
+        ChangeNotifierProvider(create: (_) => GuestLectureProvider()),
       ],
       child: Consumer<ThemeProvider>(
         builder: (context, themeProvider, _) => MaterialApp.router(
