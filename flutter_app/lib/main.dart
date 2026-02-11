22873681e31e8d61ae65b6e0fc2a8ea1f7784305
NAMATOVU-CHRISTINE <2023bcs004@std.must.ac.ug>
2026-02-11 16:17:24 +0300

    nme

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:alumni_portal/firebase_options.dart';
