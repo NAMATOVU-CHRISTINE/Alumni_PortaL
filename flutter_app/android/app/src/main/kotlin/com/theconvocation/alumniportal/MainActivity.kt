1915743f866745b727a5542eeceb0b65904aca62
NAMATOVU CHRISTINE <2023bcs004@std.must.ac.ug>
2026-05-01 21:23:55 +0300

    Add MainActivity for new package com.theconvocation.must

---
+++ flutter_app/android/app/src/main/kotlin/com/theconvocation/alumniportal/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.theconvocation.alumniportal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_app/android/app/src/main/kotlin/com/theconvocation/must/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.theconvocation.must
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
